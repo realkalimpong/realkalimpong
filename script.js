ac865031e1d5b98d69893476fbce9e46b34e3c54
realkalimpong <156909941+realkalimpong@users.noreply.github.com>
2024-03-27 16:17:48 +0530

    script.js

--- script.js
+++ script.js
@@ -26,4 +26,32 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+window.onload = function () { 
+    let slides =  
+        document.getElementsByClassName('carousel-item'); 
+  
+    function addActive(slide) { 
+        slide.classList.add('active'); 
+    } 
+  
+    function removeActive(slide) { 
+        slide.classList.remove('active'); 
+    } 
+  
+    addActive(slides[1]); 
+    setInterval(function () { 
+        for (let i = 0; i < slides.length; i++) { 
+            if (i + 1 == slides.length) { 
+                addActive(slides[0]); 
+                setTimeout(removeActive, 350, slides[i]); 
+                break; 
+            } 
+            if (slides[i].classList.contains('active')) { 
+                setTimeout(removeActive, 350, slides[i]); 
+                addActive(slides[i + 1]);
+                break; 
+            } 
+        } 
+    }, 2000); 
+};
 
